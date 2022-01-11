1ea49692945a4551c56a1d3d14eb1304679d9d49
Tanmay Shankar <tanmay.shankar@gmail.com>
2022-01-11 12:04:24 -0500

    Update save_grab_vertices.py

--- grab/save_grab_vertices.py
+++ grab/save_grab_vertices.py
@@ -12,8 +12,9 @@
 # Contact: ps-license@tuebingen.mpg.de
 #
 import sys
-sys.path.append('.')
+# sys.path.append('.')
 # sys.path.append('..')
+sys.path.insert(0,'.')
 sys.path.insert(0,'..')
 import numpy as np
 import torch
