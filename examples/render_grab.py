1cef2c58b32495f86e3222e761bb67d943c5e29c
Tanmay Shankar <tanmay.shankar@gmail.com>
2022-01-24 15:14:21 -0500

    Update render_grab.py

--- examples/render_grab.py
+++ examples/render_grab.py
@@ -181,10 +181,11 @@ if __name__ == '__main__':
     # render_sequences(cfg, seqs=[args.viz_sequence])
 
     # Now set set of sequences to visualize. 
-    sequences = ['s1/alarmclock_see_1.npz',
-                's1/train_play_1.npz',
-                's8/stamp_stamp_1.npz',
-                's10/mouse_use_1.npz']
+    # sequences = ['s1/alarmclock_see_1.npz',
+    #            's1/train_play_1.npz',
+    #            's8/stamp_stamp_1.npz',
+    #            's10/mouse_use_1.npz']
+    sequences = ['s10/cup_pour_1.npz']
     
     render_sequences(cfg, seqs=sequences)
 
